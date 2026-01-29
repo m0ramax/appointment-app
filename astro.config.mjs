ea96119d55570f5de672ad8afe4e9bd7ef3b4310
Miguel <m0ramax@MacBook-Pro-de-Miguel.local>
2026-01-29 09:22:33 -0300

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,17 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server", // Habilita SSR
-  adapter: node({
-    mode: "standalone",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    },
+    maxDuration: 8
   }),
   // Configuración para el manejo de cookies y headers
   vite: {
